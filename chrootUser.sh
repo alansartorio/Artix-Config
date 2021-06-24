55177424c2386bd41e360e874a34bc5d4b1d0dcc
Alan Sartorio <alan42ga@hotmail.com>
2021-06-23 23:35:52 -0300

    Added more package installation to scripts

--- chrootUser.sh
+++ chrootUser.sh
@@ -5,9 +5,15 @@ source <(curl -s https://raw.githubusercontent.com/alansartorio/Artix-Config/mai
 
 # Install paru
 sudo pacman -S --noconfirm --needed base-devel
+sudo pacman -S --noconfirm git
 git clone https://aur.archlinux.org/paru-bin.git paru
 ( cd paru && makepkg -si )
 rm -rf paru
+
+
+sudo pacman -S --noconfirm openssh xorg xorg-xinit wget zsh openrc-zsh-completions bspwm sxhkd feh rofi alacritty # kdeconnect connman-gtk
+paru -S --noconfirm polybar siji-git xorg-fonts-misc neovim
+sudo pacman -S neofetch # firefox chromium blender gimp
 
 sudo pacman -S --noconfirm git
 git clone https://github.com/alansartorio/dotfiles.git .dotfiles
